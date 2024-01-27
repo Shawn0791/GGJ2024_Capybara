1f3273e3844baf8ff014d11c72f06142ce46f586
Shawn <dice0791@gmail.com>
2024-01-27 12:02:21 +0000

    Orange Tree Update

--- Assets/_Shihan/Scripts/OrangeTree.cs
+++ Assets/_Shihan/Scripts/OrangeTree.cs
@@ -6,20 +6,45 @@ public class OrangeTree : MonoBehaviour
 {
     public List<GameObject> oranges = new List<GameObject>();
     public GameObject orangePrefab;
+    public float dropIntervalMin, dropIntervalMax;
+    private Coroutine dropCoroutine;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            OrangeDrop();
-        }
+
     }
-    public void OrangeDrop()
+
+    private IEnumerator RandomDropOrange()
+    {
+        float i = Random.Range(dropIntervalMin, dropIntervalMax);
+        yield return new WaitForSeconds(i);
+
+        OrangeDrop();
+
+        dropCoroutine = StartCoroutine(RandomDropOrange());
+    }
+    private void OrangeDrop()
     {
         if (oranges.Count > 0)
         {
             Instantiate(orangePrefab, oranges[0].transform.position, Quaternion.identity);
             Destroy(oranges[0].gameObject);
             oranges.Remove(oranges[0]);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ViewPort"))
+        {
+            dropCoroutine = StartCoroutine(RandomDropOrange());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ViewPort"))
+        {
+            StopCoroutine(dropCoroutine);
         }
     }
 }
